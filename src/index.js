8c16c846b49b26b19a6b5eb25f531fadf8e2f89c
John Clarence Palma <johnclarencepalma7@gmail.com>
2025-12-31 16:12:15 +0800

    update: comment on line 172

--- src/index.js
+++ src/index.js
@@ -169,7 +169,7 @@ function OpenShop({ closeHour, openHour }) {
   )
 }
 
-//render
+//render to show on page.
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<React.StrictMode><App /></React.StrictMode>)
 
